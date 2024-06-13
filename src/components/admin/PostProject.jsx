bd3e945c817459d94abe0f7975112e38d91ec2eb
Harits <haritsirawan2118@gmail.com>
2024-06-14 00:50:36 +0700

    feat: Update href for "Companys Management" in Sidebar component

--- src/components/admin/PostProject.jsx
+++ src/components/admin/PostProject.jsx
@@ -159,6 +159,73 @@ export default function PostProject() {
                       ></input>
                     </form>
                   </div>
+                  <Listbox value={selected} onChange={setSelected}>
+                    {({ open }) => (
+                      <>
+                        <Listbox.Label className="block text-base font-medium leading-6 text-gray-900">
+                          Role Project *
+                        </Listbox.Label>
+                        <div className="relative mt-2">
+                          <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            <span className="block truncate">{selected.role}</span>
+                            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                              <ChevronUpDownIcon
+                                className="h-5 w-5 text-gray-400"
+                                aria-hidden="true"
+                              />
+                            </span>
+                          </Listbox.Button>
+
+                          <Transition
+                            show={open}
+                            as={Fragment}
+                            leave="transition ease-in duration-100"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                          >
+                            <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                              {role1.map((roles) => (
+                                <Listbox.Option
+                                  key={roles.id}
+                                  className={({ active }) =>
+                                    classNames(
+                                      active ? "bg-indigo-600 text-white" : "text-gray-900",
+                                      "relative cursor-default select-none py-2 pl-3 pr-9",
+                                    )
+                                  }
+                                  value={roles}
+                                >
+                                  {({ selected, active }) => (
+                                    <>
+                                      <span
+                                        className={classNames(
+                                          selected ? "font-semibold" : "font-normal",
+                                          "block truncate",
+                                        )}
+                                      >
+                                        {roles.role}
+                                      </span>
+
+                                      {selected ? (
+                                        <span
+                                          className={classNames(
+                                            active ? "text-white" : "text-indigo-600",
+                                            "absolute inset-y-0 right-0 flex items-center pr-4",
+                                          )}
+                                        >
+                                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                        </span>
+                                      ) : null}
+                                    </>
+                                  )}
+                                </Listbox.Option>
+                              ))}
+                            </Listbox.Options>
+                          </Transition>
+                        </div>
+                      </>
+                    )}
+                  </Listbox>
                   <div className="">
                     <div className="mb-2">
                       <div className="text-base font-medium">Job Type *</div>
@@ -173,6 +240,69 @@ export default function PostProject() {
                       </button>
                     </form>
                   </div>
+                  <Listbox value={select} onChange={setSelect}>
+                    {({ open }) => (
+                      <>
+                        <Listbox.Label className="text-base font-medium leading-6 text-gray-900">
+                          Job Type *
+                        </Listbox.Label>
+                        <div className="relative items-center mt-2">
+                          <Listbox.Button className="relative w-full cursor-default rounded-md bg-abu1 border border-stroke2 py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            <span className="block truncate">{select.job}</span>
+                            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                              <ChevronUpDownIcon
+                                className="h-5 w-5 text-gray-400"
+                                aria-hidden="true"
+                              />
+                            </span>
+                          </Listbox.Button>
+
+                          <Transition
+                            show={open}
+                            as={Fragment}
+                            leave="transition ease-in duration-100"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                          >
+                            <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                              {job1.map((jobs) => (
+                                <Listbox.Option
+                                  key={jobs.id}
+                                  className={({ active }) =>
+                                    classNames(
+                                      active ? "bg-indigo-600 text-white" : "text-gray-900",
+                                      "relative cursor-default select-none py-2 pl-3 pr-9",
+                                    )
+                                  }
+                                  value={jobs}
+                                >
+                                  {({ select, active }) => (
+                                    <>
+                                      <span
+                                        className={classNames(
+                                          select ? "font-semibold" : "font-normal",
+                                          "block truncate",
+                                        )}
+                                      >
+                                        {jobs.job}
+                                      </span>
+
+                                      {select ? (
+                                        <span
+                                          className={classNames(
+                                            active ? "text-white" : "text-indigo-600",
+                                            "absolute inset-y-0 right-0 flex items-center pr-4",
+                                          )}
+                                        >
+                                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                        </span>
+                                      ) : null}
+                                    </>
+                                  )}
+                                </Listbox.Option>
+                              ))}
+                            </Listbox.Options>
+                          </Transition>
                 </div>
                 <div className="flex justify-end gap-6 pt-5">
                   <button type="button" className="bg-white text-stroke1 rounded-md py-2 px-12 hover:bg-slate-100">
